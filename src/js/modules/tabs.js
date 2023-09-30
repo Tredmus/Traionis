a9882fda101ebd6cac197f98bfd36ca3a17bb4be
JustIvo <ivaylo2001@gmail.com>
2023-09-30 20:49:43 +0300

    Add Responsive

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -7,8 +7,14 @@ $doc.on('click', '.js-tabs .js-tabs-link', event => {
 	const $wrapper = $target.closest('.js-tabs');
 	const $nav = $wrapper.find('.js-tabs-nav');
 	const $tabs = $wrapper.find('.js-tab');
-	const index = $target.closest('li', '.js-tab').index();
+	const index = $target.closest('li, .js-tab').index();
+	const alreadyCurrent = $tabs.eq(index).hasClass(classes.current);
+	const alreadyHidden = $tabs.eq(index).hasClass(classes.hidden)
+
+	if(alreadyCurrent) {
+		$tabs.eq(index).toggleClass(classes.hidden, alreadyCurrent && !alreadyHidden);
+	}
 
 	$nav.find('li').eq(index).addClass(classes.current).siblings().removeClass(classes.current);
-	$tabs.eq(index).addClass(classes.current).siblings().removeClass(classes.current);
+	$tabs.eq(index).addClass(classes.current).siblings().removeClass(classes.current).removeClass(classes.hidden);
 });
