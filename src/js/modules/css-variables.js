484ffc63646780f302416481308f39c8ee7063d7
JustIvo <ivaylo2001@gmail.com>
2023-09-30 21:40:23 +0300

    Test Viewport Height Fix

--- src/js/modules/css-variables.js
+++ src/js/modules/css-variables.js
@@ -1,7 +1,7 @@
 import { $, $win } from '../includes/globals';
 
-$win.on('load resize orientationChange updateVariables', () => {
-	$(':root').css({
-		'--vh': `${$win.innerHeight() * 0.01}px`,
-	});
-});
+// $win.on('load resize orientationChange updateVariables', () => {
+// 	$(':root').css({
+// 		'--vh': `${$win.innerHeight() * 0.01}px`,
+// 	});
+// });
