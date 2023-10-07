c02c639889cc6eff7f849e8580ab5550d0fffe14
JustIvo <ivaylo2001@gmail.com>
2023-10-08 02:03:25 +0300

    Remove Old Site

--- old-site/src/components/ArrowTop/index.js
+++
@@ -1,1 +0,0 @@
-export { default as ArrowTop } from "./ArrowTop";
--- old-site/src/components/ContactUs/validations.js
+++
@@ -1,8 +0,0 @@
-import * as Yup from "yup";
-
-export const validations = Yup.object().shape({
-  name: Yup.string().required('Name is required'),
-  email: Yup.string().email('Invalid email').required('Email is required'),
-  company: Yup.string(),
-  details: Yup.string().required('Details are required')
-});
