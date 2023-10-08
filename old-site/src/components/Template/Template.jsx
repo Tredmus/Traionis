f14f014a4157266181c817d183021df40990e6f2
JustIvo <ivaylo2001@gmail.com>
2023-10-08 17:56:53 +0300

    Remove Old Site from master branch

--- old-site/src/components/Template/Template.jsx
+++
@@ -1,7 +0,0 @@
-import classes from "./styles.module.scss";
-
-const Template = ({ className }) => {
-  return <div className={`${classes.template} ${className}`}>Template</div>;
-};
-
-export default Template;
